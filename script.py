3385af11968031b0033ce1c8c930fb45b302da92
sanjnan <34151371+sanjnan@users.noreply.github.com>
2018-05-07 18:45:27 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, jsonify, request
+#from werkzeug.wrappers import Request, Response
+import re
+#import json
+#from pprint import pprint
+
+app =Flask(__name__)
+
+#@Request.application
+#def application(request):
+#    return Response('Hello World!')
+#
+#
+@app.route('/plot/')
+def plot():
+    from pandas_datareader import data
+    import datetime
+    from bokeh.plotting import figure, output_file, show
+    from bokeh.embed import components
+    from bokeh.resources import CDN
+
+    start=datetime.datetime(2016,10,1)
+    end=datetime.datetime(2017,3,10)
+
+    df=data.DataReader(name="GOOG",data_source="google",start=start,end=end)
+
+    df_bull=df[df.Close>=df.Open]
+    df_bear=df[df.Open>df.Close]
+
+    p=figure(x_axis_type='datetime', height=300, width=1000,title='Candlestick Chart',sizing_mode='scale_width')
+    #p.grid.grid_line_alpha=0.5
+#hi
+    hours_12=12*60*60*1000
+
+    p.segment(df.index, df.High, df.index, df.Low, color='black')
+    p.rect(x=df_bull.index,y=((df_bull.Open+df_bull.Close)/2),
+           width=hours_12,height=abs(df_bull.Close-df_bull.Open),color='#CCFFFF',line_color='black')
+
+    p.rect(x=df_bear.index,y=((df_bear.Open+df_bear.Close)/2),
+           width=hours_12,height=abs(df_bear.Open-df_bear.Close),color='#FF3333',line_color='black')
+
+    script1,div1=components(p)
+    cdn_js=CDN.js_files[0]
+    cdn_css=CDN.css_files[0]
+    return render_template("plot.html",
+    script1=script1, div1=div1,cdn_css=cdn_css,cdn_js=cdn_js)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/ome/',methods=["GET"])
+def geti():
+    resp = {"username": "kkk"}
+    return jsonify(resp)
+
+@app.route('/ost/',methods=["POST"])
+def posti():
+    responseId = request.json["responseId"]
+    req = request.json
+    for header in req:
+        if header == "queryResult":
+            for item in req[header]:
+                if item == "intent":
+                    for option in req[header][item]:
+                        if option == "displayName":
+                            if req[header][item][option] == "SharingMobile":
+                                for itm in req[header]:
+                                    if itm == "parameters":
+                                        for para in req[header][itm]:
+                                            if para == "mobilenumber":
+                                                p = re.compile(r'^[6789]\d{9}$',re.I|re.M)
+                                                print (req[header][itm][para])
+                                                if p.match(str(req[header][itm][para])):
+                                                    respo = {"fulfillmentText": "Please enter the OTP received","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                                                    return jsonify(respo)
+                                                else:
+                                                    respo = {"fulfillmentText": "Please enter a valid 10 digit mobile","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                                                    return jsonify(respo)
+                            respo = {"fulfillmentText": "","fulfillmentMessages": [],"source": "example.com","payload": {},"outputContexts": [ ],"followupEventInput": {}}
+                            return jsonify(respo)
+    #session = request.json["session"]
+    #querytext = request.json["querytext"]
+    #mobilenumber = request.json["mobilenumber"]
+
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+@app.route('/policy/')
+def policy():
+    return render_template("policy.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    #from werkzeug.serving import run_simple
+    #run_simple('localhost', 4000, application)
